9c19fe24e4c7c6a0ab754ceed5c1ca9789d4a8fe
Ciprian Stefan <steffanciprian@gmail.com>
2021-03-26 05:21:59 -0700

    al 4lea commit//workWithStateAndEvents

--- App.js
+++ App.js
@@ -2,14 +2,26 @@ import React, {useState} from 'react';
 import {Button, StyleSheet, TextInput, View} from 'react-native';
 
 export default function App() {
-    const [outputText, setOutputText] = useState('Cipii e cel mai tare!')
+    const [enteredGoal, setEnteredGoal] = useState('');
+    const [goals, setGoals] = useState([]);
+
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText);
+    }
+
+    const addGoalHandler = () => {
+        setGoals(goals => [...goals, enteredGoal]);
+    }
+
     return (
         <View style={styles.screenStyle}>
-            <View style={styles.inputContainerStyle}
-            >
-                <TextInput style={styles.textInputStyle}
+            <View style={styles.inputContainerStyle}>
+                <TextInput
+                    style={styles.textInputStyle}
+                    onChangeText={goalInputHandler}
+                    value={enteredGoal}
                 />
-                <Button title="Add"/>
+                <Button title="Add" onPress={addGoalHandler}/>
             </View>
         </View>
     );
@@ -31,9 +43,6 @@ const styles = StyleSheet.create({
         padding: 10,
         textAlign: "center",
     },
-    buttonStyle:{
-
-    }
 
 
 });
