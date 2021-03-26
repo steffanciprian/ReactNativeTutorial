d83265ad124e0f5bb8d13062804470cfac84a3df
Ciprian Stefan <steffanciprian@gmail.com>
2021-03-26 04:36:50 -0700

    al treilea commit //styleObjects

--- App.js
+++ App.js
@@ -1,51 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React , {useState} from 'react';
-import { StyleSheet, TextInput, View , Button, Platform} from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {useState} from 'react';
+import {StyleSheet, TextInput, View, Button, Platform} from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Cipii e cel mai tare!')
-  return (
-      <View style={
-          {padding : 50}
-      }>
-          <View style={
-              {
-              flexDirection: "row",
-              justifyContent: 'space-between',
-              alignItems: "center"
-              }}
-          >
-              <TextInput
-                  placeholder="Input"
-                  style={{
-                      width:"80%",
-                      borderBottomWidth: 1,
-                      borderColor: "black",
-                      padding: 10,
-                      textAlign:"center",
-                  }}
-              />
-              <Button title="Add"/>
-          </View>
-      </View>
-  );
+    const [outputText, setOutputText] = useState('Cipii e cel mai tare!')
+    return (
+        <View style={styles.screenStyle}>
+            <View style={styles.inputContainerStyle}
+            >
+                <TextInput style={styles.textInputStyle}
+                />
+                <Button title="Add"/>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-      ...Platform.select({
-          ios: {
-              backgroundColor:"pink"
-          },
-          android:{
-              backgroundColor: "pink"
-          },
-          default:{
-              backgroundColor:"blue"
-          }
-      })
-  }
+    screenStyle: {
+        padding: 50
+    },
+    inputContainerStyle: {
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        alignItems: "center"
+    },
+    textInputStyle: {
+        width: "80%",
+        borderBottomWidth: 1,
+        borderColor: "black",
+        padding: 10,
+        textAlign: "center",
+    },
+    buttonStyle:{
+
+    }
+
+
 });
